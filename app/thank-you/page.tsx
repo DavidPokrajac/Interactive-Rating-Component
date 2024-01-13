0a7e32f0197486b7c9cce29f1f36f2d34116f445
David <d0pokrajac@gmail.com>
2024-01-13 10:07:39 +0100

    Initial commit

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,14 @@
+export default function ThankYou() {
+    return (
+        <main>
+            <article>
+                <span>You selected out of 5</span>
+                <p>Thank you!</p>
+                <p>
+                    We appreciate you taking the time to give a rating. If you
+                    ever need more support, don’t hesitate to get in touch!
+                </p>
+            </article>
+        </main>
+    );
+}
