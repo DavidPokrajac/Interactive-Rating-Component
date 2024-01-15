d92bab8be7504301474c434a06888e528e2cf078
David <d0pokrajac@gmail.com>
2024-01-15 16:50:22 +0100

    Added BEM naming convention

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./styles.module.css";
 
 export default function Home() {
     return (
         <main>
-            <article>
-                <h1>How did we do?</h1>
-                <p>
+            <article className="article">
+                <h1 className="article__heading">How did we do?</h1>
+                <p className="article__intro">
                     Please let us know how we did with your support request. All
                     feedback is appreciated to help us improve our offering!
                 </p>
-                <div>
-                    <span>1</span>
-                    <span>2</span>
-                    <span>3</span>
-                    <span>4</span>
-                    <span>5</span>
+                <div className="article__ratings">
+                    <span className="article__ratings__rating-grade">1</span>
+                    <span className="article__ratings__rating-grade">2</span>
+                    <span className="article__ratings__rating-grade">3</span>
+                    <span className="article__ratings__rating-grade">4</span>
+                    <span className="article__ratings__rating-grade">5</span>
                     <button type="submit">Submit</button>
                 </div>
             </article>
--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,9 +1,11 @@
 export default function ThankYou() {
     return (
         <main>
-            <article>
-                <span>You selected out of 5</span>
-                <p>Thank you!</p>
+            <article className="thank-you-modal">
+                <span className="thank-you-modal__result">
+                    You selected out of 5
+                </span>
+                <p className="thank-you-modal__thanks">Thank you!</p>
                 <p>
                     We appreciate you taking the time to give a rating. If you
                     ever need more support, don’t hesitate to get in touch!
