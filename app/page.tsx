fa5188047bc5e5fdb334e5664c85831f924b113a
David <d0pokrajac@gmail.com>
2024-01-21 19:56:38 +0100

    Added styles for desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,39 @@ import styles from "./styles.module.css";
 export default function Home() {
     return (
         <main>
-            <article className="article">
-                <h1 className="article__heading">How did we do?</h1>
-                <p className="article__intro">
+            <article className={styles.article}>
+                <figure className={styles.article__figure}>
+                    <Image
+                        width={16}
+                        height={17}
+                        src="/images/icon-star.svg"
+                        alt=""
+                    />
+                </figure>
+                <h1 className={styles.article__heading}>How did we do?</h1>
+                <p className={styles.article__intro}>
                     Please let us know how we did with your support request. All
                     feedback is appreciated to help us improve our offering!
                 </p>
-                <div className="article__ratings">
-                    <span className="article__ratings__rating-grade">1</span>
-                    <span className="article__ratings__rating-grade">2</span>
-                    <span className="article__ratings__rating-grade">3</span>
-                    <span className="article__ratings__rating-grade">4</span>
-                    <span className="article__ratings__rating-grade">5</span>
-                    <button type="submit">Submit</button>
+                <div className={styles.article__ratings}>
+                    <span className={styles.article__ratings__ratingGrade}>
+                        1
+                    </span>
+                    <span className={styles.article__ratings__ratingGrade}>
+                        2
+                    </span>
+                    <span className={styles.article__ratings__ratingGrade}>
+                        3
+                    </span>
+                    <span className={styles.article__ratings__ratingGrade}>
+                        4
+                    </span>
+                    <span className={styles.article__ratings__ratingGrade}>
+                        5
+                    </span>
+                    <button className={styles.button} type="submit">
+                        Submit
+                    </button>
                 </div>
             </article>
         </main>
